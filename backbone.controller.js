dd32795f0964eb803f96f3733572cee64d05859a
Michael Welborn <6305265+mawelborn@users.noreply.github.com>
2018-07-31 21:25:30 -0500

    Create backbone.controller.js

---
+++ backbone.controller.js
@@ -0,0 +1,11 @@
+Backbone.Controller = function(options) {
+    options || (options = {});
+    this.initialize.apply(this, arguments);
+};
+
+_.extend(Backbone.Controller.prototype, Backbone.Events, {
+    initialize: function() {},
+    terminate: function() { this.stopListening(); }
+});
+
+Backbone.Controller.extend = Backbone.Router.extend;
