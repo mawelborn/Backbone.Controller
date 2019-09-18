3db94ba34082c9a0274014b6470b0d804ec9348e
Michael Welborn <6305265+mawelborn@users.noreply.github.com>
2019-09-18 15:26:32 -0500

    Add UMD, ESLint, terminate trigger and minified file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Backbone.Controller
-A simple container to organize controller methods in Backbone. It's purpose is to provide a designated place to put code that orchestrates models, collections, and views so that it's not littered across views and routers.
+A simple container to organize controller methods in Backbone.
+It's purpose is to provide a designated place to put code that
+orchestrates models, collections, and views so that it's not
+littered across views and routers.
--- backbone.controller.js
+++ backbone.controller.js
@@ -1,4 +1,13 @@
-(function() {
+/*! Backbone.Controller.js 1.1.0 | (c) Michael Welborn | Backbone.Controller is MIT licensed */
+(function(root, factory) {
+    "use strict";
+    if (typeof define === "function" && define.amd)
+        define(["underscore", "backbone"], factory);
+    else if (typeof exports !== "undefined")
+        factory(require("underscore"), require("backbone"));
+    else
+        factory(root._, root.Backbone);
+})(typeof self !== "undefined" ? self : this, function(_, Backbone) {
     "use strict";
 
     Backbone.Controller = function(options) {
@@ -8,8 +17,12 @@
 
     _.extend(Backbone.Controller.prototype, Backbone.Events, {
         initialize: function() {},
-        terminate: function() { this.stopListening(); }
+
+        terminate: function() {
+            this.trigger("terminate");
+            this.stopListening();
+        }
     });
 
     Backbone.Controller.extend = Backbone.Router.extend;
-}());
+});
